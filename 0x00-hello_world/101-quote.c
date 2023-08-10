8e388510cceaaa7da4ce74ccaf987258115b5422
Tahani-Saber <tahani.saber.habib11@gmail.com>
2023-08-10 15:46:52 -0700

    second advaced task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - start point of the program
+ *
+ * Return: always must be 1 as required
+ *
+ * no need to return keyword
+*/
+
+int main(void)
+{
+char ch = 'and that piece of art is useful\" - Dora Korpar, 2015-10-19';
+putcher(ch);
+return (1);
+}
