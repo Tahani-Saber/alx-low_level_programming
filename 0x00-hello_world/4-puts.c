a31ccea355ef57d24b3805dfb52334c729b699e9
Tahani-Saber <tahani.saber.habib11@gmail.com>
2023-08-10 10:41:51 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
-\**
-*main - entry point
-*
-*Description : A C program that prints with put function 
-*
-*Return: alwaye 0 (success)  
-*\
-int main(void){
-       	puts("\"Programming is like building a multilingual puzzle"):
-       	return (0):
+
+/**
+ * main - Entry point
+ *
+ * Description : A C program that prints with put function 
+ *
+ * Return: Alwaye 0 (success)  
+*/
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
