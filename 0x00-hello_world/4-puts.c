c8018deb7c75979bb7fe99f646467f599b26bef7
Tahani-Saber <tahani.saber.habib11@gmail.com>
2023-08-10 11:43:28 -0700

    task 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
-\**
-*main - entry point
-*
-*Description : A C program that prints with put function 
-*
-*Return: alwaye 0 (success)  
-*\
-int main(void){
-       	puts("\"Programming is like building a multilingual puzzle"):
-       	return (0):
+/**
+ * main - entry point
+ *
+ * Description : A C program that prints with put function
+ *
+ * Return: alwaye 0 (success)
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <std.io>
+
+/**
+ * main :The Start of the prog.
+ * Description : The program will print with printf function
+ * Return : Always return 0 (success)
+*/
+
+int main(void)
+{
+printf ("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
