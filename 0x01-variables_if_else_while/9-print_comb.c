e67b1b042978c21c899cac897d06ac86a533add8
Tahani-Saber <tahani.saber.habib11@gmail.com>
2023-08-14 13:09:39 -0700

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - start of the prog
+ *
+ * Description: A C program to print combination of nums
+ *
+ * Return: return 0
+*/
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		putchar(x);
+		putchar(" \n");
+	}
+	putchar('$');
+	return (0);
+}
