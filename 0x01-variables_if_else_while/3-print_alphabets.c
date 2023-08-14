274b241abfbe6dabd2e11b6c6aab62419ade6acc
Tahani-Saber <tahani.saber.habib11@gmail.com>
2023-08-14 07:34:35 -0700

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main -start of the program
+ *
+ * Description: A c program to print the alphabet
+ *
+ * Return: return 0
+*/
+
+int main(void)
+{
+	int x;
+
+	for (x = 'A'; x <= 'Z'; x++)
+	{
+		putchar(tolower(x));
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
