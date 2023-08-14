367481da54be9e512f7f6ab54a4ef93cf8bdb30a
Tahani-Saber <tahani.saber.habib11@gmail.com>
2023-08-14 13:52:28 -0700

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -15,8 +15,9 @@ int main(void)
 	for (x = '0'; x <= '9'; x++)
 	{
 		putchar(x);
-		putchar(' ');
-		putchar(',');
+		if(x < 9)
+			putchar(',');
+			putchar(' ');
 	}
 	putchar('\n');
 	return (0);
