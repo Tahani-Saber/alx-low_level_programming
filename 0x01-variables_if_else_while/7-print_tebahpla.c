3ca384f9e7a403c63562ba043f0a1c392a55b162
Tahani-Saber <tahani.saber.habib11@gmail.com>
2023-08-14 12:37:15 -0700

    task 7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <ctype.h>
 
 /**
  * main - start of the program
@@ -14,7 +15,7 @@ int main(void)
 
 	for (x = 'Z'; x >= 'A'; x--)
 	{
-		putchar(x);
+		putchar(tolower(x));
 	}
 	putchar('\n');
 	return (0);
