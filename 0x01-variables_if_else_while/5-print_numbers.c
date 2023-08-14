a3740786d7fd8f1a92aeadf8d0f71d69097f895e
Tahani-Saber <tahani.saber.habib11@gmail.com>
2023-08-14 09:21:07 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *  main - start of the program
+ *
+ *  Description: A C program to print the numbres of the decimal system
+ *
+ *  Return: return 0
+*/
+
+int main(void)
+{
+	int x;
+
+	for (x = 0 ; x <= 9 ; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
