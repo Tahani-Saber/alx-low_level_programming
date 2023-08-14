11a173fbf02a8e9a15cab0b976686844851fe312
Tahani-Saber <tahani.saber.habib11@gmail.com>
2023-08-14 12:55:57 -0700

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - start of the program
+ *
+ * Description: A C program to print the hexadecimal
+ *
+ * Return: return 0
+*/
+
+int main(void)
+{
+	char c;
+	char a = '0';
+
+	while (a <= 9)
+	{
+		putchar(a);
+		a++;
+	}
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
+
