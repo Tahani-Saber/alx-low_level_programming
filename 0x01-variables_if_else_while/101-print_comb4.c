288ab29aebe56876ad7941675ed90d85f382a9e6
Tahani-Saber <tahani.saber.habib11@gmail.com>
2023-08-14 18:06:48 -0700

    advanced task 2

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - start of the prog
+ *
+ * Description: A C program
+ *
+ * Return: return 0
+*/
+
+int main(void)
+{
+	int i = 0;
+
+	while (i < 1000)
+	{
+		if (i % 10 != i / 10 % 10 && i % 10 != i / 100 && i / 10 % 10 != i / 100)
+		{
+			putchar(i / 100 + '0');
+			putchar(i / 10 % 10 + '0');
+			putchar(i % 10 + '0');
+			if (i < 789)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		i++;
+	}
+	return (0);
+}
