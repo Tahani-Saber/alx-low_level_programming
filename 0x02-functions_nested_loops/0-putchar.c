3d1ffd26beb11dcf838a1beab200786c6ec00c1d
Tahani-Saber <tahani.saber.habib11@gmail.com>
2023-08-15 09:10:43 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - start of the prog
+ *
+ * Description: A C program to print txt
+ *
+ * Return: return 0
+*/
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
