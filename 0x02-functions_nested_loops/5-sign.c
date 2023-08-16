811d4110d74f85d9014b3e6498bea70dc66943cb
Tahani-Saber <tahani.saber.habib11@gmail.com>
2023-08-15 19:48:08 -0700

    task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_sign - main function
+ *
+ * Description: prog to check the sign of the number
+ *@n: the parameter express the number
+ *
+ * Return: return 0 or 1
+*/
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	if else(n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+		(n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+}
