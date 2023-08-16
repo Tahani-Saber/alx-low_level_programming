baf079bdcbb1580651d601c38038d00133845ed1
Tahani-Saber <tahani.saber.habib11@gmail.com>
2023-08-15 17:22:00 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,21 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - start of the prog
+ * main - start of the prg
  *
- * Description: A C program to print txt
+ * Description: A c program to prnt text with specific prototype
  *
  * Return: return 0
 */
 
 int main(void)
 {
-	printf("_putchar\n");
-	return (0);
+	char str[] = "_putchar";
+	int ch;
+
+	for (ch = 0; ch < 8; ch++)
+	{
+		_putchar(str[ch]);
+		_putchar('\n');
+	}
 }
