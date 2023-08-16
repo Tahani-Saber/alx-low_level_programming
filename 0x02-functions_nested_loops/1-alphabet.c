459190d2b630b109da5486c1d0a06056d9f056f0
Tahani-Saber <tahani.saber.habib11@gmail.com>
2023-08-15 18:14:31 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet - entry point
+ *
+ * Description: This function prints the alphabet, in lowercase
+ *
+ * Return: void
+*/
+
+void print_alphabet(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+	return (0);
+}
