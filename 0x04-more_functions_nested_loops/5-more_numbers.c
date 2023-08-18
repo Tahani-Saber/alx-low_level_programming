a395ee132689506cd176e72b1b76a952de40748d
Tahani-Saber <tahani.saber.habib11@gmail.com>
2023-08-18 09:45:47 -0700

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,12 +12,15 @@ void more_numbers(void)
 {
 	int i, num;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0 ; i < 10; i++)
 	{
-		for (num = 0; num <= 14; num++)
+		for (j = 0; j <= 14; j++)
 		{
-			_putchar(num + '0');
+			if (j >= 10)
+				_putchar(j / 10 + '0');
+				_putchar(j % 10 + '0');
 		}
+		_putchar('\n');
+ 
 	}
-	_putchar('\n');
 }
