dbb81137ebde399da8e3fa66c9dd9524ca82e41e
Tahani-Saber <tahani.saber.habib11@gmail.com>
2023-08-18 14:08:04 -0700

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -17,7 +17,7 @@ void more_numbers(void)
 		for (num = 0; num <= 14; num++)
 		{
 			if (num >= 10)
-				_putchar(num / 10 + '0');
+				_putchar(num + '0');
 			else
 				_putchar(num % 10 + '0');
 		}
