35a4e67516efdb04941717b6dc0ebf84cc130466
Tahani-Saber <tahani.saber.habib11@gmail.com>
2023-08-18 09:26:25 -0700

    task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - check the code
+ *
+ * Description: to print the numbers more times
+ *
+ * Return: void
+*/
+
+void more_numbers(void)
+{
+	int i, num;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (num = 0; num <= 14; num++)
+		{
+			_putchar(num);
+		}
+		_putchar(num);
+	}
+	_putchar('\n');
+}
