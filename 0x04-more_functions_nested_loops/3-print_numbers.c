dacabf267f00876c600f3284bddd89241f120b3e
Tahani-Saber <tahani.saber.habib11@gmail.com>
2023-08-18 08:52:27 -0700

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - check the code
+ *
+ * Description: to print numbers
+ *
+ * return: void
+*/
+
+void print_numbers(void)
+{
+	int num;
+
+	for (num >= 0; num <= 9; num++)
+	{
+		_putchar(num);
+	}
+	_putchar('\n');
+}
