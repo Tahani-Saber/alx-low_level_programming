fa7ab850fdb859d7289dd1ccc9c14db2d25abe05
Tahani-Saber <tahani.saber.habib11@gmail.com>
2023-08-18 18:18:45 -0700

    task 9x

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Description: a program to print text instead of specific numbers
+ *
+ * Return: return void
+*/
+
+int main (void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+			_putchar("Fizz");
+		else if (i % 5 == 0)
+			_putchar("Buzz");
+		else if (i % 3 == 0 && i % 5 == 0)
+			_putchar("FizzBuzz");
+		else
+			_putchar(i);
+	}
+}
