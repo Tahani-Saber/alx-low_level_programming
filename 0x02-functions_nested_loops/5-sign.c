7d932f407b1c9b6000b7dbf2411cfd0f56bbe290
Tahani-Saber <tahani.saber.habib11@gmail.com>
2023-08-20 03:27:09 -0700

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,19 +11,19 @@
 
 int print_sign(int n)
 {
-	if (n > '0')
+	if (n > 0)
 	{
 		_putchar('+');
 		return (1);
 	}
-	else if (n < '0')
+	else if (n < 0)
 	{
 		_putchar('-');
 		return (-1);
 	}
-	else (n == '0')
+	else (n == 0)
 	{
-		_putchar(48);
+		_putchar(0);
 		return (0);
 	}
 }
