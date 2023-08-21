cd9dfe2c642b7113bbcdcafc09d8bd64dc5d73f1
Tahani-Saber <tahani.saber.habib11@gmail.com>
2023-08-21 11:46:15 -0700

    task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * swap_int - check the code
+ * @a: the frist pointer
+ * @b: the second pointer
+ * Return: return void
+*/
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
