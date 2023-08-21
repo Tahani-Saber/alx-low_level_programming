d96de7d9e413ffffa69c47b9c08da7dbb169663d
Tahani-Saber <tahani.saber.habib11@gmail.com>
2023-08-21 14:19:11 -0700

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts - check the code
+ * @str: string to be printed
+ *
+ * Return: return void
+*/
+
+void _puts(char *str)
+{
+	int a = 0;
+
+	while (str[i] != '\0')
+	{
+		putchar(str[i]);
+		i++;
+	}
+	putchar('\n');
+}
