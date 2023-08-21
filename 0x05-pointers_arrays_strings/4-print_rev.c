30e89b46f7578936f0d803bf14d9fc85e43b7a99
Tahani-Saber <tahani.saber.habib11@gmail.com>
2023-08-21 14:35:03 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - check the code
+ * @s: the function parameter
+ *
+ * Return: return void
+*/
+
+void print_rev(char *s)
+{
+	int count, i;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	for (count = i - 1; count >= i; i--)
+	{
+		putchar(str[i]);
+	}
+	putchar('\n');
+}
