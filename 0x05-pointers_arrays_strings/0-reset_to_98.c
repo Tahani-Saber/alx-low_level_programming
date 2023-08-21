24806f0086b13f9970ab66960d2fd241b53ff81f
Tahani-Saber <tahani.saber.habib11@gmail.com>
2023-08-21 10:55:23 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98  - main function of the code
+ * @*n: point to n address
+ * @n: parameter
+ *
+ * Return: return void
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
