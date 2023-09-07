8e67f27ee1bc343f3272837b5dd926bef003798e
Tahani-Saber <tahani.saber.habib11@gmail.com>
2023-09-07 13:55:42 -0700

    task o of the malloc and free function

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,16 +1,17 @@
 #include "main.h"
 
 /**
- * create_array - create a an array of chars, and initializes it with a specific char
+ * create_array - create a an array of chars
+ * , and initializes it with a specific char
  * @size: the size of the arr
- * @c: the initial value of the arr 
+ * @c: the initial value of the arr
  *
  * Return: return the pointer of the arr or NULL if it fail
 */
 
 char *create_array(unsigned int size, char c)
 {
-	char* array;
+	char *array;
 	unsigned int i;
 
 	if (size == 0)
