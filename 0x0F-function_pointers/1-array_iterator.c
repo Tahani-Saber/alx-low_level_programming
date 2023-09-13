cc62dfa3001b43ba8bf84bf50ea1f4664e2b2f51
Tahani-Saber <tahani.saber.habib11@gmail.com>
2023-09-13 08:21:45 -0700

    function named with action to excute array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterators - function to executes a function given
+ * as a parameter on each element of an array
+ * @array: frist argu array to excute the fun
+ * @size: size of the array
+ * @action: second argu function we need to excute
+ * REturn: return void
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t s;
+
+	if (array && action)
+		s = 0;
+		while (s < size)
+			action(array[s]);
+			s++;
+}
