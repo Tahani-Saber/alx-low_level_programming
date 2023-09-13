19c967abd4c6895ce6db59e827b419f564e85931
Tahani-Saber <tahani.saber.habib11@gmail.com>
2023-09-13 07:46:14 -0700

    update

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,5 @@
-#ifndef FUNCTION_POINTERS
-#define FUNCTION_POINTERS
+#ifndef FUNCTION_POINTERS_
+#define FUNCTION_POINTERS_
 
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
