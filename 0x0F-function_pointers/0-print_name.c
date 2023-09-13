29fbbffb441e28ff8ebd5097aaed81b40b67b45f
Tahani-Saber <tahani.saber.habib11@gmail.com>
2023-09-13 06:58:22 -0700

    print name function

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_name -function to print the person name
+ * @name: the first parameter person name
+ * @f: the second parameter function of the name
+ *
+ * Return: return void
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL || f != NULL)
+		f(name);
+}
