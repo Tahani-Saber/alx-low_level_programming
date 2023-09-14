5f21f3c03a90326bb62f79c3e0496b2b04c55968
Tahani-Saber <tahani.saber.habib11@gmail.com>
2023-09-14 14:36:04 -0700

    sum them all task 0

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,9 +1,10 @@
-#ifndef VARIADIC_FUNCTION.H
-#define VARIADIC_FUNCTION.H
+#ifndef VARIADIC_FUNCTIONS.H
+#define VARIADIC_FUNCTIONS.H
 
 #include <stdarg.h>
 #include <stdio.h>
 
+_putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 
 
