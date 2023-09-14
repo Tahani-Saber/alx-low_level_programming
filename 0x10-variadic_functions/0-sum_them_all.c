72de18a5b2cb0b2a0a6edfb90cc4354e01d046e2
Tahani-Saber <tahani.saber.habib11@gmail.com>
2023-09-14 15:02:59 -0700

    update

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -5,28 +5,25 @@
  * of numbers
  * @n: the fixed argument of our variadic function
  *
- * Return: return sum
+ * Return: always 0
  */
 
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list args;
-	int s = 0;
+	int s;
 	unsigned int i;
 	
 	va_start(args, n);
 
-	if (n != 0)
-	{
+	if (n == 0)
+		return (0);
+
 		for (i = 0; i < n; i++)
 		{
-			int x = va_arg(args, int);
-			
-			s += x;
+			 s += va_arg(args, int);
 		}
 		va_end(args);
 		return (s);
 	}
-	if (n == 0)
-		return (0);
 }
