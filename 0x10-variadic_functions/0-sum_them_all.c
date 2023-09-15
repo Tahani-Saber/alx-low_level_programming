58adf2e00bd2f76107fbbd0f3ad77d401a4d0260
Tahani-Saber <tahani.saber.habib11@gmail.com>
2023-09-14 17:20:20 -0700

    completion of task 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,14 +10,14 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-	int s;
-	unsigned int i;
 	va_list args;
-
-	va_start(args, n);
+	 int s = 0;
+	unsigned int i;
 
 	if (n == 0)
 		return (0);
+
+	va_start(args, n);
 
 	for (i = 0; i < n; i++)
 	{
